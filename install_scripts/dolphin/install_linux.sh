3f4ddb7c4de1ae7404f10a78191aa4e80ac392fd
George Dubuque <gtdubuque@gmail.com>
2026-06-14 20:58:10 -0700

    deleted the config.cfg file added linux changes

---
+++ install_scripts/dolphin/install_linux.sh
@@ -0,0 +1,7 @@
+SCRIPT_PATH="$1"
+INSTALL_DIR="$2"
+echo $SCRIPT_PATH
+curl -L -o emulators/dolphin/dolphin.AppImage "https://github.com/pkgforge-dev/Dolphin-emu-AppImage/releases/download/2603a%402026-06-07_1780858798/Dolphin_Emulator-2603a-anylinux-x86_64.AppImage"
+echo "butt hole"
+chmod +x emulators/dolphin/dolphin.AppImage
+
